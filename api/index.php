dfd8ea17cf588768d183a29a206492f34d3d2336
rafimumtaz <rafimumtaz86@gmail.com>
2026-06-11 10:40:40 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -6,18 +6,12 @@ define('LARAVEL_START', microtime(true));
 
 require __DIR__.'/../vendor/autoload.php';
 
-// Vercel build puts absolute paths in the cache (/vercel/path0) which breaks at runtime (/var/task)
-$cacheFiles = [
-    __DIR__.'/../bootstrap/cache/packages.php',
-    __DIR__.'/../bootstrap/cache/services.php',
-    __DIR__.'/../bootstrap/cache/config.php',
-    __DIR__.'/../bootstrap/cache/routes-v7.php',
-];
-foreach ($cacheFiles as $file) {
-    if (file_exists($file)) {
-        @unlink($file);
-    }
-}
+// Override cache paths to /tmp so Laravel doesn't use the frozen build-time absolute paths!
+$_ENV['APP_SERVICES_CACHE'] = '/tmp/storage/bootstrap/cache/services.php';
+$_ENV['APP_PACKAGES_CACHE'] = '/tmp/storage/bootstrap/cache/packages.php';
+$_ENV['APP_CONFIG_CACHE'] = '/tmp/storage/bootstrap/cache/config.php';
+$_ENV['APP_ROUTES_CACHE'] = '/tmp/storage/bootstrap/cache/routes-v7.php';
+$_ENV['APP_EVENTS_CACHE'] = '/tmp/storage/bootstrap/cache/events.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
@@ -32,6 +26,7 @@ $directories = [
     "$storagePath/framework/sessions",
     "$storagePath/framework/views",
     "$storagePath/logs",
+    "$storagePath/bootstrap/cache",
 ];
 
 foreach ($directories as $directory) {
