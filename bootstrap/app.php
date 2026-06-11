0d0e01836c8a1a6f71d9826191b251a61ffff6a8
rafimumtaz <rafimumtaz86@gmail.com>
2026-06-11 10:55:24 +0700

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Trust Vercel's reverse proxy so HTTPS and Asset URLs work properly!
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
