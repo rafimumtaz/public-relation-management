9fab22670a3c2859087649a0ce2b9521de826340
rafimumtaz <rafimumtaz86@gmail.com>
2026-06-12 08:51:53 +0700

    Update BrandAssetsTable.php

--- app/Filament/Resources/BrandAssets/Tables/BrandAssetsTable.php
+++ app/Filament/Resources/BrandAssets/Tables/BrandAssetsTable.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\BrandAssets\Tables;
 
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\DeleteAction;
 use Filament\Actions\EditAction;
 use Filament\Actions\ViewAction;
 use Filament\Tables\Columns\ImageColumn;
@@ -46,6 +47,7 @@ class BrandAssetsTable
             ->recordActions([
                 ViewAction::make(),
                 EditAction::make(),
+                DeleteAction::make(),
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
